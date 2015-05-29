3326b5710a9c4bda47627628ad273c2220d54ff3
DoctorGoat <thearcy@gmail.com>
2015-05-29 19:09:08 -0400

    WinSize
    A tool to resize windows using the Win32 API.

---
+++ winSize/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace winSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.winList = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.resizeButton = new System.Windows.Forms.Button();
+            this.resButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "x";
+            // 
+            // winList
+            // 
+            this.winList.FormattingEnabled = true;
+            this.winList.Location = new System.Drawing.Point(12, 12);
+            this.winList.Name = "winList";
+            this.winList.Size = new System.Drawing.Size(286, 21);
+            this.winList.TabIndex = 3;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(304, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(66, 23);
+            this.refreshButton.TabIndex = 4;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // resizeButton
+            // 
+            this.resizeButton.Location = new System.Drawing.Point(148, 44);
+            this.resizeButton.Name = "resizeButton";
+            this.resizeButton.Size = new System.Drawing.Size(75, 23);
+            this.resizeButton.TabIndex = 5;
+            this.resizeButton.Text = "Resize";
+            this.resizeButton.UseVisualStyleBackColor = true;
+            this.resizeButton.Click += new System.EventHandler(this.resizeButton_Click);
+            // 
+            // resButton
+            // 
+            this.resButton.Location = new System.Drawing.Point(229, 44);
+            this.resButton.Name = "resButton";
+            this.resButton.Size = new System.Drawing.Size(141, 23);
+            this.resButton.TabIndex = 6;
+            this.resButton.Text = "Fit Windows resolution";
+            this.resButton.UseVisualStyleBackColor = true;
+            this.resButton.Click += new System.EventHandler(this.resButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 83);
+            this.Controls.Add(this.resButton);
+            this.Controls.Add(this.resizeButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.winList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "GoatResize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox winList;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button resizeButton;
+        private System.Windows.Forms.Button resButton;
+    }
+}
+
---
+++ winSize/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace winSize
+{
+    //winSize is a tool for resizing another window.
+    //The games Diablo 3 and Guild Wars 2 both have windowed mode,
+    //but no support for setting the window to an arbitrary size.
+    //Thus, this tool was created.
+
+
+    public partial class Form1 : Form
+    {
+        //initialize, refresh available windows
+        //Does not get current window for some reason?
+        public Form1()
+        {
+            InitializeComponent();
+            getTitles();
+        }
+
+        //Import functions MoveWindow and FindWindow from the Windows API
+        //TODO: Get window bounds so window doesn't move when resized
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        //Clear the item list. Then, get a list of processes and use the process.MainWindowTitle 
+        //list to populate the window list. Select the first item.
+        private void getTitles()
+        {
+            winList.Items.Clear();
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                if (process.MainWindowTitle != "")
+                {
+                    winList.Items.Add(process.MainWindowTitle);
+                }
+            }
+            winList.SelectedItem = winList.Items[0];
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            getTitles();
+        }
+
+        //Find the window with the desired title.
+        //Then, call MoveWindow and set its x and y values to
+        //the textbox's specifications.
+        private void resizeButton_Click(object sender, EventArgs e)
+        {
+            int xValue = 0;
+            int yValue = 0;
+            try
+            {
+                xValue = Convert.ToInt32(textBox1.Text);
+                yValue = Convert.ToInt32(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid x/y sizes.");
+            }
+            try
+            {
+                if (xValue > 0 && yValue > 0)
+                {
+                    IntPtr thisWin = FindWindow(null, winList.Text);
+                    MoveWindow(thisWin, 0, 0, xValue, yValue, true);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Could not resize window:" + winList.Text);
+            }
+        }
+
+        //Same as above, but sets the window's resolution to the screen's resolution. 
+        //Also happens if you try to set a window to larger than the screen resolution above.
+
+        //TODO: Possibly remove the borders to make an implementation of borderless fullscreen?
+        private void resButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IntPtr thisWin = FindWindow(null, winList.Text);
+                MoveWindow(thisWin, 0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, true);
+            }
+            catch
+            {
+                MessageBox.Show("Could not resize window:" + winList.Text);
+            }
+
+        }
+    }
+
+
+}
